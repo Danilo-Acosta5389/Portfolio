c0e058a7f3a776fdb7ce09bcc56e24fd628ee529
Danilo Acosta <danilo.acostadote@chasacademy.se>
2025-02-16 07:06:00 +0100

    Update homepage text for branding and engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <main className="dark text-foreground bg-background h-screen flex flex-col justify-center items-center font-[family-name:var(--font-geist-sans)]">
       <div className=" space-y-8">
         <h1 className=" text-4xl font-bold text-center sm:text-left">
-          Hello from portfolio
+          Hello from portf0lio
         </h1>
         <p className="  text-lg text-center sm:text-left">
-          This project is coming soon
+          This project is coming s👀n
         </p>
       </div>
     </main>
