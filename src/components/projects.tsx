13fac5c9cd2e043d18f6a1de8dde617d2bea0236
Danilo Acosta <danilo.acostadote@chasacademy.se>
2025-02-19 04:09:28 +0100

    Add Contact and Projects components; update Hero and Header for navigation

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useThemeContext } from "@/context/theme-context";
 import React from "react";
 
 export default function Projects() {
-  return <section className="w-full h-screen bg-red-900"></section>;
+  const { theme } = useThemeContext();
+  return (
+    <section
+      id="projects"
+      className={`${theme} h-screen w-full bg-background text-foreground font-mono flex flex-col justify-center items-center`}
+    >
+      <h1 className="text-2xl font-mono">Project section</h1>
+    </section>
+  );
 }
